490010aedb28fc7a7688f48b042f5895dee3bf00
tamilov3 <tamaranaumovic@gmail.com>
2020-12-07 20:41:00 +0100

    dodato objašnjenje za api.php

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
+
+| Dodavanje API ruta za projekat
 |
 */
 
